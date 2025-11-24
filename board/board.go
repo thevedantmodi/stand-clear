d39c50827c2a6d78c03567c4d4eaccafc39f226d
Vedant Modi <vedantmodi@gmail.com>
2025-11-24 09:23:53 -0800

    ADD support for shuttles

--- board/board.go
+++ board/board.go
@@ -60,6 +60,10 @@ var Terminals = map[string][2]string{
 	"J":  {"Jamaica Center-Parsons/Archer", "Broad St"},
 	"Z":  {"Jamaica Center-Parsons/Archer", "Broad St"},
 	"L":  {"8 Av", "Canarsie-Rockaway Pkwy"},
+	"GS": {"Grand Central-42 St", "Times Sq-42 St"},
+	"FS": {"Franklin Av", "Prospect Park"},
+	"H":  {"Broad Channel", "Rockaway Park"},
+
 	/* TODO add shuttles */
 }
 
